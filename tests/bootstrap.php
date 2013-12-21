0f2fdc4e5a3709e64854d33b5041ec2a35eafd59
@twingo.b <twingo.b@gmail.com>
2013-12-21 07:43:42 +0000

    add 5.3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,4 @@
 
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /** @var $loader \Composer\Autoload\ClassLoader */
-$loader->add('Twingob\TravisCIChallenge', [__DIR__]);
+$loader->add('Twingob\TravisCIChallenge', array(__DIR__));
